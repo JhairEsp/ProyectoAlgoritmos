8352e07452f84014afd73bf27f1b0c1c19347247
JhairEsp <126709022+JhairEsp@users.noreply.github.com>
2023-09-10 16:16:07 -0500

    Add files via upload

---
+++ src/main/java/com/mycompany/proyectoalgoritmos/ProyectoAlgoritmos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectoalgoritmos;
+
+/**
+ *
+ * @author Jhair
+ */
+public class ProyectoAlgoritmos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
