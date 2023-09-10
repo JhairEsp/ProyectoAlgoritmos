68428433004b2b1aed0cdec8433186ba4e6c97d5
JhairEsp <jhairoswaldo@gmail.com>
2023-09-10 16:31:56 -0500

    Cambios prueba

--- ProyectoAlgoritmos/src/main/java/MetodoBurbuja/MetodoBurbuja.java
+++ ProyectoAlgoritmos/src/main/java/MetodoBurbuja/MetodoBurbuja.java
@@ -11,5 +11,6 @@ package MetodoBurbuja;
 public class MetodoBurbuja {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("Cambios");
     }
 }
